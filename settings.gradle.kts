0ab7690308964a32d1807d931415d9ad6e6a2339
yasindevtech-a11y <yasindev.tech@gmail.com>
2026-08-01 06:28:37 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // jcifs-ng için
+    }
+}
+
+rootProject.name = "VaultSync"
+include(":app")
