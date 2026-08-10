2fc930af73739d6f8b53a2303b2c00c7d6f6310d
yasindevtech-a11y <yasindev.tech@gmail.com>
2026-08-10 14:15:39 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.work:work-runtime-ktx:2.9.1")
     implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
 }
